5d9fb4356612bfece3c174bf8abc754d7a06e901
ryuyauetake <ryuya@anymindgroup.com>
2021-03-20 19:02:49 +0900

    feat(usermodal): add Ok,cancel button
    these function stub

--- src/components/abstracts/modal/UserModal.tsx
+++ src/components/abstracts/modal/UserModal.tsx
@@ -1,12 +1,24 @@
 import style from './UserModal.module.css'
 
 export const UserModal = (props: {toggleMySelf: () => void}) => {
+    const okFn = () => {
+        window.alert('ok');
+        props.toggleMySelf();
+    }
+    const cancelFn = () => {
+        window.alert('cancel');
+        props.toggleMySelf();
+    }
     return (
         <div className={style.spaShellModal}>
             <div className={style.spaShellModalContent}>
-                <p>Modal</p>
                 <p onClick={props.toggleMySelf}>X</p>
             </div>
+            <div>
+                <input type="text"/>
+            </div>
+            <button onClick={okFn}>OK</button>
+            <button onClick={cancelFn}>Cancel</button>
         </div>
     );
 }

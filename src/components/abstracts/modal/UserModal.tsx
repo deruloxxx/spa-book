0380468db63b115aeb6723d22f730f59f8cbe20f
ryuyauetake <ryuya@anymindgroup.com>
2021-03-20 18:32:56 +0900

    Fix compile error

--- src/components/abstracts/modal/UserModal.tsx
+++ src/components/abstracts/modal/UserModal.tsx
@@ -1,20 +1,11 @@
 import style from './UserModal.module.css'
 
-export function _UserModal (boo) {
-    return "<div>" + "Modal value is: " + boo.toggleMySelf() + "</div>"
-}
-
-_UserModal({toggleMySelf: function () {return "hello"}})
-
-export const UserModal = (props: {toggleMySelf?: () => void}) => {
-    console.log(props);
-
-    const toggleMySelf = props.toggleMySelf || (() => window.alert("hello"))
+export const UserModal = (props: {toggleMySelf: () => void}) => {
     return (
         <div className={style.spaShellModal}>
             <div className={style.spaShellModalContent}>
                 <p>Modal</p>
-                <p onClick={toggleMySelf}>X</p>
+                <p onClick={props.toggleMySelf}>X</p>
             </div>
         </div>
     );
--- src/components/mols/LoginButton/LoginButton.tsx
+++ src/components/mols/LoginButton/LoginButton.tsx
@@ -4,12 +4,11 @@ import {UserModal} from "../../abstracts/modal/UserModal";
 
 export const LoginButton = () => {
     const [isModal, toggleModal] = useToggle(false);
-
     return (
         <>
             <div className="login-button">
-                <div onClick={toggleModal} style={{padding: '2em'}}>
-                    <p>login button</p>
+                <div onClick={toggleModal}>
+                    <p>Please</p>
                 </div>
             </div>
             {isModal &&

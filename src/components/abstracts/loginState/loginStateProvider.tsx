34020dfb48d060fe6dd212b2b763d134389d82c8
ryuyauetake <ryuya@anymindgroup.com>
2021-03-27 17:03:06 +0900

    Update yarn

--- src/components/abstracts/loginState/loginStateProvider.tsx
+++ src/components/abstracts/loginState/loginStateProvider.tsx
@@ -2,7 +2,6 @@ import {MyContext} from  './loginStateContext';
 import {ReactChild} from  'react';
 
 export function LoginStateProvider(props:{children: ReactChild}) {
-
     return <MyContext.Provider value={{
         isLogin: false,
         login: () => { window.alert('this is stub') },
